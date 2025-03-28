15dc8d328977d9ec574369f019128fb306852ec8
ashwz <146004307+ashwz@users.noreply.github.com>
2025-03-28 16:06:08 -0400

    Add files via upload

--- example_dtlcor.R
+++ example_dtlcor.R
@@ -13,8 +13,10 @@ document("dtlcor")
 require(dtlcor)
 ls(getNamespace("dtlcor"))
 
+set.seed(1000)
+
 # set design parameters
-nsim      = 1000  
+nsim      = 100
 
 delta     = 0.05  
 n         = 80    
@@ -28,17 +30,17 @@ D           = 162
 mPFS        = c(180, 276, 300)
 q           = c(0.2, 0.4, 0.5)
 gamma       = 0.15
-drop_rate   = 0.05
-enroll      = 20 * 12 / 365.25
+drop_rate   = 0.05 # annual drop-out rate
+enroll      = 20 * 12 # annual enrollment rate
 interim_t   = c(0.5, 1)
 fix_rho     = NULL
 
 # --------------------basic function-----------------
 # theoretical FWER
-dtl_tier_the(delta, n, t = 1, rho = 0.4, q = 0.3, alpha_s = alpha)
+dtl_tier_the(n, t = 1, rho = 0.4, q = 0.3, alpha_s = alpha, delta)
 
 # get alpha_s given alpha
-dtl_get_alpha_s(delta, n, t = 1, rho = 0.4, q = 0.3, alpha)
+dtl_get_alpha_s(n, t = 1, rho = 0.4, q = 0.3, alpha, delta)
 
 # get upper bound of correlation
 dtl_cor_the_PH_upper_bound(tau_k = n/N, pi_ar = 0.5, q = 0.3, gamma = 0.2)
@@ -46,16 +48,20 @@ dtl_cor_the_PH_upper_bound(tau_k = n/N, pi_ar = 0.5, q = 0.3, gamma = 0.2)
 # --------------------real application-----------------
 # get alpha_t (minimum of alpha_s)
 # values of fix_rho: NULL (use upper bound of rho) or real values from 0 to 1.
-dtl_app_get_alpha_t(n, N, delta, q_seq, gamma_seq, alpha, fix_rho = NULL)$rst_alpha_t[1,]
-dtl_app_get_alpha_t(n, N, delta, q_seq, gamma_seq, alpha, fix_rho = 1)$rst_alpha_t[1,]
-dtl_app_get_alpha_t(n, N, delta, q_seq, gamma_seq, alpha, fix_rho = 0)$rst_alpha_t[1,]
+dtl_app_get_alpha_t(n, N, q_seq, gamma_seq, alpha, fix_rho = NULL, delta)$rst_alpha_t[1,]
+dtl_app_get_alpha_t(n, N, q_seq, gamma_seq, alpha, fix_rho = 1, delta)$rst_alpha_t[1,]
+dtl_app_get_alpha_t(n, N, q_seq, gamma_seq, alpha, fix_rho = 0, delta)$rst_alpha_t[1,]
+
+dtl_app_get_alpha_t_sim(nsim = 100000, n, N, q_seq, gamma_seq, alpha, fix_rho = NULL, delta = delta)$rst_alpha_t[1,]
+dtl_app_get_alpha_t_sim(nsim = 100000, n, N, q_seq, gamma_seq, alpha, fix_rho = 1, delta = delta)$rst_alpha_t[1,]
+dtl_app_get_alpha_t_sim(nsim = 100000, n, N, q_seq, gamma_seq, alpha, fix_rho = 0, delta = delta)$rst_alpha_t[1,]
 
 # simulate a single trial
-dtl_single = dtl_app_sim_single(D, N, n, mPFS, q, gamma, delta, drop_rate, enroll, interim_t)
+dtl_single = dtl_app_sim_single(D, N, n, mPFS, q, gamma, drop_rate, enroll, interim_t, delta = delta)
 
 # simulation results
-alpha_t     = dtl_app_get_alpha_t(n, N, delta, q_seq, gamma_seq, alpha, fix_rho = NULL)$rst_alpha_t$alpha_t[1]
-dtl_summary = dtl_app_sim(nsim, alpha_t, D, N, n, mPFS, q, gamma, delta, drop_rate, enroll, interim_t)
+alpha_t     = dtl_app_get_alpha_t(n, N, q_seq, gamma_seq, alpha, fix_rho = NULL, delta)$rst_alpha_t$alpha_t[1]
+dtl_summary = dtl_app_sim(nsim, alpha_t, D, N, n, mPFS, q, gamma, drop_rate, enroll, interim_t, delta = delta)
 
 # -------------------open the R shiny-----------------
 dtl_shiny()
